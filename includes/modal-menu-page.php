ea483cef0df8d195d7972005b711903504d6d757
Neven Mendrila <nevenmendrila@gmail.com>
2018-05-16 14:47:10 +0200

    Converts index.html to .php and adds includes

---
+++ includes/modal-menu-page.php
@@ -0,0 +1,29 @@
+<div class="modal-page modal-page-1 modal-menu-page">
+    <ul class="modal-menu">
+        <li>
+            <span class="number">01</span>
+            <span class="modal-menu-item">About Ray-<br />dio K</span>
+        </li>
+        <li>
+            <span class="number">02</span>
+            <span class="modal-menu-item">Credits to music makers</span>
+        </li>
+        <li>
+            <span class="number">03</span>
+            <span class="modal-menu-item">Do<br />-nate</span>
+        </li>
+        <li>
+            <span class="number">04</span>
+            <span class="modal-menu-item">Imprint</span>
+        </li>
+        <li>
+            <span class="number">05</span>
+            <span class="modal-menu-item">App</span>
+        </li>
+        <li>
+            <span class="number">06</span>
+            <span class="modal-menu-item">Cont-<br />act</span>
+        </li>
+    </ul>
+    <span class="modal-copyright">Copyright (C) 2018 Raydio Kirtan</span>
+</div>
