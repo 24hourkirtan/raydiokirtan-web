8fb5f639ba208133a3586a3be1aed6f6e19fbcca
Neven Mendrila <nevenmendrila@gmail.com>
2018-05-17 12:35:39 +0200

    Hides the intro heading when modal is open

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,21 @@
 $(document).ready(function() {
     $('.menu-line-toggle').on('click', function() {
-        $('.modal').fadeToggle();
+        var modal = $('.modal');
+        modal.fadeToggle().toggleClass('modal-open');
+        
+        if (modal.hasClass('modal-open')) {
+            $('.intro-heading').animate({'opacity': 0}, 200);
+        } else {
+            $('.intro-heading').animate({'opacity': 1}, 200);
+        }
+        
+        
     });
     
     $('.modal-menu-item').on('click', function() {
         var item = $(this);
         var itemLink = item.attr('data-link');
-        
+
         $('.modal-page').fadeOut();
         $('.modal-page-' + itemLink).fadeIn();
     });
